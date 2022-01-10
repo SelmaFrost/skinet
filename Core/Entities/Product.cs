3b0afeb21d76eeb5d0c749fae8dae41849f44a74
Selma Frost Øgaard <selmafo@uio.no>
2022-01-10 12:54:56 +0100

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; } //primary key
+
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StoreContex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class StoreContex : DbContext
+
+    {
+        public StoreContex(DbContextOptions<StoreContex> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
