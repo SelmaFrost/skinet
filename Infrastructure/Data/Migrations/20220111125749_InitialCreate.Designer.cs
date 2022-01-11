bbc73894fde2a159cddbed0b82af2eaf4915318d
Selma Frost Øgaard <selmafo@uio.no>
2022-01-11 15:01:22 +0100

    End of section 3

--- Infrastructure/Data/Migrations/20220111125749_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20220111125749_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContex))]
-    partial class StoreContexModelSnapshot : ModelSnapshot
+    [Migration("20220111125749_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
